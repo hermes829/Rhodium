d0002a23f570a85c83b4e4d516f6fba587732241
Ben Radford <benjamin.radford@gmail.com>
2013-06-22 17:26:04 -0400

    Make some maps

--- R/geodistance.R
+++ R/geodistance.R
@@ -7,6 +7,7 @@
 # and longitude using the Haversine #
 # formula (assumes spherical earth) #
 #####################################
+
 geodistance <- function(lat1,lon1,lat2,lon2,r=6371)
 {
   dLat <- deg2rad(lat2-lat1)
@@ -14,9 +15,9 @@ geodistance <- function(lat1,lon1,lat2,lon2,r=6371)
   lat1 <- deg2rad(lat1)
   lat2 <- deg2rad(lat2)
   
-  a = sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat1) * cos(lat2)
-  c = 2 * atan2(sqrt(a), sqrt(1-a)) 
-  d = r * c
+  a <- sin(dLat/2) * sin(dLat/2) + sin(dLon/2) * sin(dLon/2) * cos(lat1) * cos(lat2)
+  c <- 2 * atan2(sqrt(a), sqrt(1-a)) 
+  d <- r * c
   return(d)
 }
 deg2rad <- function(deg)
---
+++ R/opendata.R
@@ -0,0 +1,43 @@
+# Clearing workspace
+rm(list=ls())
+
+# Setting working directory
+if(Sys.info()["user"]=="janus829")
+{pathMain="~/Desktop/Research/Rhodium/R";
+ pathGraphics="~/Dropbox/Research/Rhodium/Graphics";
+ pathFunctions="~/Desktop/Prog Notes/R Functions"}
+# Setting working directory
+if(Sys.info()["user"]=="Ben")
+{pathMain="C:/Users/Ben/Dropbox/Rhodium/R";
+ pathGraphics="C:/Users/Ben/Dropbox/Rhodium/Graphics";
+ pathFunctions="C:/Users/Ben/Documents/Github/Rhodium/R";
+ pathData="C:/Users/Ben/Dropbox/Rhodium/Data"
+}
+
+# Concatenation operator
+"+" <- function(...) UseMethod("+")
+"+.default" <- .Primitive("+")
+"+.character" <- function(...) paste(...,sep="")
+
+library(maptools)
+library(RColorBrewer)
+library(raster)
+library(rgdal)
+source(pathFunctions+"/geodistance.R")
+
+prio <- read.csv(pathData+"/PRIO - Conflict Site Data/ConflictSite 4-2010_v3 Dataset.csv")
+#eigdp <- readAsciiGrid(pathData+"/Earth Institute - GDP Grids/gdp90_15mi.asc")
+eigdp <- raster(pathData+"/Earth Institute - GDP Grids/gdp90_15m.tif")
+oilgas <- readShapeSpatial(pathData+"/Horn - Giant Fields Data/Giant_Fields_Data.shp")
+world <- readShapeSpatial(pathData+"/TM_WORLD_BORDERS_SIMPL-0.2.shp")
+
+mypalatte <- colorRampPalette(c("#C7E9C0","#005A32"))
+arg <- list(at=c(0,9), labels=c("Low","High"))
+par(mar=c(1,1,1,1))
+plot(log(eigdp),col=mypalatte(12),add=F,interpolate=T,breaks=0:9,axis.args=arg,frame=F,bty="n",xaxt="n",yaxt="n")
+rect(-180,-90,180,90, col="gray", border=NA)
+plot(world,col="white",border="gray",add=T)
+plot(log(eigdp),col=mypalatte(12),add=T,interpolate=T,breaks=0:9,axis.args=arg,frame=F,bty="n",xaxt="n",yaxt="n")
+plot(world,border="gray",add=T)
+points(oilgas,pch=16,col="#0000FF55",cex=.5)
+points(prio$Longitude,prio$Latitude,pch=16,cex=prio$Radius/700,col="#FF000055")
