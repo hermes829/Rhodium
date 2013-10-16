d99356becd9901ffbc3f4cbb3bcfe86c45258693
Ben Radford <benjamin.radford@gmail.com>
2013-10-16 10:47:40 -0400

    Two functions for calculating distance
    Haversine and Vincenty

--- R/geodistance.R
+++ R/geodistance.R
@@ -8,6 +8,8 @@
 # formula (assumes spherical earth) #
 #####################################
 
+require("geosphere")
+
 geodistance <- function(lat1,lon1,lat2,lon2,r=6371)
 {
   dLat <- deg2rad(lat2-lat1)
@@ -24,3 +26,15 @@ deg2rad <- function(deg)
 {
   return(deg*(pi/180))
 }
+
+vincenty <- function(lat1,lon1,lat2,lon2,unit="km")
+{
+  coords1 <- data.frame(lon=lon1,lat=lat1)
+  coords2 <- data.frame(lon=lon2,lat=lat2)
+  result <- distVincentyEllipsoid(coords1,coords2)
+  if(unit=="km")
+    result <- result/1000
+  return(result)
+}
+
+vincenty(c(-16.499,35.9886),c(-68.146,78.9072),c(35.318,35.7789),c(-82.461,78.8003))
