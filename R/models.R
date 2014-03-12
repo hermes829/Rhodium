a39146770632caa29eea1df4b65b743eefbdee1f
benradford <benjamin.radford@gmail.com>
2014-03-12 15:19:26 -0400

    Model updates, slight outline updates

--- R/models.R
+++ R/models.R
@@ -8,25 +8,50 @@ load('combinedData.rda')
 yData$upperincome=0
 yData$upperincome[which(yData$income_l0 %in% c('High income: OECD', 'High income: nonOECD'  ) )] = 1
 
-##################################################################
-# SAVE THIS JUST IN CASE EVERYTHING ELSE FALLS APART
+
+# CREATE APPROPRIATE VARIABLES FOR REGRESSIONS
+####################################################################################################################
 yData$lnminDist.min <- log(yData$minDist.min)
 yData$lnminDist.mean <- log(yData$minDist.mean)
 yData$lncapDist.min <- log(yData$capDist.min)
 yData$lnArea <- log(yData$Conflict.area.mean)
 yData$Int.max <- yData$Int.max-1
 yData$intPerKm <- yData$Int.max/yData$lnArea
+yData$USA <- yData$ccode==2
+yData$coldwar <- yData$year<1991
 
 yData <- yData[order(yData$year),]
+####################################################################################################################
 
+
+## MODELS FOR GDP GROWTH PER CAPITA (ANNUAL %)
+####################################################################################################################
 model1 <- lmer(NY.GDP.PCAP.KD.ZG_l0 ~ upperincome + Int.max + lnArea + lnminDist.min + territorial.max + (1|ccode), data=yData)
 summary(model1)
 plot(resid(model1))
 
-model2 <- lmer(NY.GDP.PCAP.KD.ZG_l0 ~ upperincome + Int.max + lnArea + lnminDist.min + territorial.max + Int.max*lnminDist.min + (1|ccode), data=yData)
-summary(model2)
-plot(resid(model2))
-# This model shows us that: High intensity conflicts result in higher growth if they are farther from the nearest city.
+# model2 <- lmer(NY.GDP.PCAP.KD.ZG_l1 ~ upperincome + Int.max + lnArea + lnminDist.min + territorial.max + Int.max*lnminDist.min + (1|ccode), data=yData)
+# summary(model2)
+# plot(resid(model2))
+# This model is weird
+####################################################################################################################
 
-##################################################################
+
+## MODELS FOR GDP GROWTH (ANNUAL %)
+####################################################################################################################
+plot(density(yData$NY.GDP.MKTP.KD.ZG_l0,na.rm=T))
+
+# yData$GDP_transform_l0 <- sqrt(abs(yData$NY.GDP.MKTP.KD.ZG_l0))
+# yData$GDP_transform_l0 <- yData$GDP_transform_l0 * ifelse(yData$NY.GDP.MKTP.KD.ZG_l0<0,-1,1)
+# plot(density(yData$GDP_transform_l0,na.rm=T))
+
+model3 <- lmer(NY.GDP.MKTP.KD.ZG_l0 ~ upperincome + Int.max + lnArea + lnminDist.min + territorial.max + (1|ccode), data=yData)
+summary(model3)
+plot(resid(model3),col=model3@frame$ccode)
+text(1:length(resid(model3)),resid(model3),model3@frame$ccode,cex=.5)
+# model4 <- lmer(NY.GDP.MKTP.KD.ZG_l1 ~ upperincome + Int.max + lnArea + lnminDist.min + territorial.max + Int.max*lnminDist.min + (1|ccode), data=yData)
+# summary(model4)
+# plot(resid(model4))
+####################################################################################################################
+
 
