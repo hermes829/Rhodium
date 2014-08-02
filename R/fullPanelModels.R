822c667fc10e98b87f3291c67b229093a1a6a24d
hermes829 <hermes829@gmail.com>
2014-08-01 23:42:36 -0400

    cleaning

--- R/fullPanelModels.R
+++ R/fullPanelModels.R
@@ -64,6 +64,65 @@ rmse(capMod)
 ###################################################################
 
 ###################################################################
+# Coefficient Plots
+setwd(pathMain)
+source('vizResults.R')
+
+results=summary(cityMod)$'coefficients'
+# results=summary(capMod)$'coefficients'
+
+coefs=rownames(results)[2:nrow(results)]
+vnames=c('Ln(GDP per capita)$_{t-1}$', 'Ln(Land Area)$_{t}$',
+	'Ln(Inflation)$_{t-1}$', 'Conflict Intensity$_{t-1}$',
+	'Ln(1/Conflict Area)$_{t-1}$','Conflict Duration$_{t}$',
+	'Number of Conflicts$_{t}$', 
+	'Ln(1/Min. City Conflict)$_{t-1}$')
+	# 'Ln(1/Min. Cap. Conflict)$_{t-1}$')
+
+temp = ggcoefplot(coefData=results, 
+    vars=coefs, varNames=vnames, Noylabel=FALSE, coordFlip=TRUE,
+    colorGrey=FALSE, grSTA=0.5, grEND=0.1)
+temp
+# setwd(pathTex)
+# tikz(file='mod3CoefPlot.tex', width=4, height=4, standAlone=F)
+# temp
+# dev.off()
+###################################################################
+
+###################################################################
+# Simulations
+coefs=rownames(results)[2:nrow(results)]
+data=na.omit(impData[,coefs])
+results=cityMod
+
+estimates = results@beta
+names(estimates)=rownames(summary(results)$'coefficients')
+varcov = vcov(results)
+RSS = sum(residuals(results)^2)
+dfResid = nrow(data) - length(results@beta) - 
+	nrow(coefficients(results)$'ccode') + 1
+# error = sqrt(RSS/dfResid) 
+error=0 # Set to zero to get rid of fundamental uncertainty
+toTest = coefs[length(coefs)]
+quants=quantile(data[,toTest], seq(0,1,.1))
+tRange=seq(quants[1],quants[length(quants)], 0.1)
+
+temp = ggsimplot(sims=10000, simData=data, vars=coefs, 
+  vi=toTest, vRange=tRange, ostat=median,
+  betas=estimates, vcov=varcov, sigma=error, intercept=TRUE,
+  ylabel="\\% $\\Delta$ GDP$_{t}$", 
+  xlabel="Ln(1/Min. City Conflict.)$_{t-1}$",
+  # xlabel="Ln(1/Min. Cap. Conflict.)$_{t-1}$",  
+  plotType='ribbon'
+  )
+temp
+# setwd(pathTex)
+# tikz(file='mod3SimPlot.tex', width=6, height=4, standAlone=F)
+# temp
+# dev.off()
+###################################################################
+
+###################################################################
 # Divide into train and test
 train=impData[which(impData$year %in% 1989:2005),]
 test=na.omit(impData[which(impData$year %in% 2006:2008),])
@@ -91,68 +150,11 @@ rmse(capModTrain)
 
 # Calculate predicted values for test data
 
-# results=summary(cityModTrain)$'coefficients'
-results=summary(capModTrain)$'coefficients'
+results=summary(cityModTrain)$'coefficients'
+# results=summary(capModTrain)$'coefficients'
 
 tData=cbind('(Intercept)'=1,test[,rownames(results)[2:nrow(results)]])
 tPreds = data.matrix(tData) %*% cbind(results[,1])
 tDiff = sqrt( mean( (test[,'lnGDPgr'] - tPreds)^2 ) )
 tDiff
-###################################################################
-
-###################################################################
-# Coefficient Plots
-setwd(pathMain)
-source('vizResults.R')
-
-results=summary(cityMod)$'coefficients'
-coefs=rownames(results)[2:nrow(results)]
-vnames=c('Ln(GDP per capita)$_{t-1}$', 'Ln(Land Area)$_{t}$',
-	'Ln(Inflation)$_{t-1}$', 'Conflict Intensity$_{t-1}$',
-	'Ln(1/Conflict Area)$_{t-1}$','Conflict Duration$_{t}$',
-	'Number of Conflicts$_{t}$', 
-	'Ln(1/Min. City Conflict)$_{t-1}$')
-	# 'Ln(1/Min. Cap. Conflict)$_{t-1}$')
-
-temp = ggcoefplot(coefData=results, 
-    vars=coefs, varNames=vnames, Noylabel=FALSE, coordFlip=TRUE,
-    colorGrey=FALSE, grSTA=0.5, grEND=0.1)
-temp
-# setwd(pathTex)
-# tikz(file='mod3CoefPlot.tex', width=4, height=4, standAlone=F)
-# temp
-# dev.off()
-###################################################################
-
-###################################################################
-# Simulations
-coefs=rownames(results)[2:nrow(results)]
-data=na.omit(impData[,coefs])
-results=capMod
-
-estimates = results@beta
-names(estimates)=rownames(summary(results)$'coefficients')
-varcov = vcov(results)
-RSS = sum(residuals(results)^2)
-dfResid = nrow(data) - length(results@beta) - 
-	nrow(coefficients(results)$'ccode') + 1
-# error = sqrt(RSS/dfResid) 
-error=0 # Set to zero to get rid of fundamental uncertainty
-toTest = coefs[length(coefs)]
-quants=quantile(data[,toTest], seq(0,1,.1))
-tRange=seq(quants[1],quants[length(quants)], 0.1)
-
-temp = ggsimplot(sims=10000, simData=data, vars=coefs, 
-  vi=toTest, vRange=tRange, ostat=median,
-  betas=estimates, vcov=varcov, sigma=error, intercept=TRUE,
-  ylabel="\\% $\\Delta$ GDP$_{t}$", 
-  xlabel="Ln(1/Min. City Conflict.)$_{t-1}$",
-  # xlabel="Ln(1/Min. Cap. Conflict.)$_{t-1}$",  
-  plotType='ribbon'
-  )
-temp
-# setwd(pathTex)
-# tikz(file='mod3SimPlot.tex', width=6, height=4, standAlone=F)
-# temp
-# dev.off()
 ###################################################################
